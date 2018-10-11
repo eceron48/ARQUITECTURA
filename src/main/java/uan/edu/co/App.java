4d6f978f252eabf30de4ce63bd070c3b4c3355b3
Esteban Salas <estebassguitrar@gmail.com>
2018-10-10 20:36:07 -0500

    hello

---
+++ src/main/java/uan/edu/co/App.java
@@ -0,0 +1,15 @@
+package uan.edu.co;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    
+}
